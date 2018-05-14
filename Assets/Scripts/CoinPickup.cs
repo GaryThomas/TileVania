f23127d29c086c2ce31ab880bdffe8d859799997
Gary Thomas <gary@mlbassoc.com>
2018-05-14 09:16:23 +0100

    Add simple [coin] pickup along with sound effect

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+	[SerializeField] AudioClip coinPickupSFX;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		AudioSource.PlayClipAtPoint (coinPickupSFX, Camera.main.transform.position);
+		Destroy (gameObject);
+	}
+}
